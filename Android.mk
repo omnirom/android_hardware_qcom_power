eb56562b06657c6ac4c5ea3f0bc8c97be6476b08
Shareef Ali <shareefalis@cyanogenmod.org>
2014-06-11 00:11:58 -0400

    qcom_power: add TARGET_PROVIDES_POWERHAL
    Change-Id: I97b908622f8b5ea3b75658d8a118d988c909697f

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
@@ -17,4 +18,5 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     LOCAL_MODULE_TAGS := optional
     include $(BUILD_SHARED_LIBRARY)
   endif
+endif
 endif
